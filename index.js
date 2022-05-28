a0a8ccfc53c4485879275216cc059a0d8fe83405
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2022-05-28 16:31:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,48 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 const fs = require("fs");
-require('child_process').execSync("cls", {stdio: 'inherit'});
+require('child_process').execSync("cls", {
+  stdio: 'inherit'
+});
 console.log("nodecli ALPHA please visit documentation before use: [insert link here]");
 
 function usrcmd() {
   readline.question(`> `, command => {
-    commandex(command);
+      commandex(command);
   });
 }
 
-var currentPath = "C:/Users/Theo/Desktop";
+var currentPath = __dirname;
 
 usrcmd();
 
 function commandex(command) {
-  switch (command) {
-    case 'exit':
-      console.log("Exiting nodecli...");
-      process.exit(22);
-    case 'dir':
-      fs.readdirSync(currentPath).forEach(file => {
-      console.log(file);
-    });
-    break;
-    default:
-      console.log(`Sorry, command "${command}" not found.`);
+  switch (command.substring(0, 2)) {
+      case 'ex':
+          console.log("Exiting nodecli...");
+          process.exit(22);
+      case 'ls':
+          fs.readdirSync(currentPath).forEach(file => {
+              console.log(file);
+          });
+          break;
+      case 'cf':
+          fs.writeFileSync(command.split(" ").pop(), '');
+          console.log(`created "${command.split(" ").pop()}"`);
+          break;
+      case 'rf':
+          try {
+              fs.unlinkSync(command.split(" ").pop());
+              console.log("File is deleted.");
+          } catch (error) {
+              console.log(error);
+          }
+          break;
+      case 'wp':
+        console.clear();
+        break;
+      default:
+          console.log(`Sorry, command "${command}" not found.`);
   }
-    usrcmd();
+  usrcmd();
 }
