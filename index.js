9e93d47402a395ac4d8241745a1a511d410d7dd9
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2022-05-28 15:22:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const fs = require("fs");
+require('child_process').execSync("cls", {stdio: 'inherit'});
+console.log("nodecli ALPHA please visit documentation before use: [insert link here]");
 
-console.clear()
+function usrcmd() {
+  readline.question(`> `, command => {
+    commandex(command);
+  });
+}
 
-readline.question(`> `, command => {
-  commandex(command)
-});
+var currentPath = "C:/Users/Theo/Desktop";
+
+usrcmd();
 
 function commandex(command) {
-    if (command == "cd") {
-      console.log("ok")
-    } else {
-      console.log("not ok");
-    }
-    readline.question(`> `, command => {
-        commandex(command)
-      });
+  switch (command) {
+    case 'exit':
+      console.log("Exiting nodecli...");
+      process.exit(22);
+    case 'dir':
+      fs.readdirSync(currentPath).forEach(file => {
+      console.log(file);
+    });
+    break;
+    default:
+      console.log(`Sorry, command "${command}" not found.`);
+  }
+    usrcmd();
 }
